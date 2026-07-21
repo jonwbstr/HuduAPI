242b025009dc10a4ed4593dd215ccdaf91609fd5
jonwbstr <jon.wbstr@gmail.com>
2026-07-21 16:15:01 -0400

    Revert "Add Archived parameter for website archiving"

--- HuduAPI/Public/Set-HuduWebsite.ps1
+++ HuduAPI/Public/Set-HuduWebsite.ps1
@@ -39,9 +39,6 @@ function Set-HuduWebsite {
     .PARAMETER EnableSPF
     When true, SPF monitoring is enabled.
 
-    .PARAMETER Archived
-    When true, website is archived.
-
     .PARAMETER Slug
     Url identifier
 
@@ -82,8 +79,6 @@ function Set-HuduWebsite {
 
         [Alias('enable_spf')]
         [String]$EnableSPF = '',
-
-        [String]$Archived = '',
 
         [string]$Slug
     )
@@ -136,10 +131,6 @@ function Set-HuduWebsite {
 
     if ($EnableSPF) {
         $Website.website.enable_spf_tracking = $EnableSPF
-    }
-    
-    if ($Archived) {
-        $Website.website.archived = $Archived
     }
 
     $JSON = $Website | ConvertTo-Json -Depth 10
